4aa30bcdc8b516c217a15d9ba6f1ad5092d8a3d0
Undo1 <parker@erwaysoftware.com>
2014-09-23 19:48:22 -0600

    initial commit

---
+++ privacypolicy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Privacypolicy::Application.config.secret_token = '6f3894e4afd0570c9e53d282274e939ec533f3ee82be13987a41a6092112e574d313dc8aa9829c892ed11449c0964f7cea9264a2d462d42e4af999dff7e1afa0'
